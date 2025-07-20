7b2d57a7e9a42725fd8c2b9234a44e758de3694c
glyph8 <newdy0808@gmail.com>
2025-07-20 21:54:56 +0900

    refactor: reset.css 적용, mainlayout main 바깥  div 추가

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -1,11 +1,20 @@
+import UpperNav from "../../components/UpperNav";
 
-const MyPage = () =>{
-
-
-    return(
-        <>
-            프로필 페이지
-        </>
+const MyPage = () => {
+    return (
+        <div className="w-full h-full">
+            <UpperNav text="회원정보" />
+            <div className="bg-white">
+                <nav className="flex flex-col justify-start items-start mt-8">
+                    <div className="w-full h-12 text-Schemes-On-Surface text-xl font-semibold font-['Pretendard'] leading-7">
+                        회원가입
+                    </div>
+                    <div className="w-full h-8 text-black text-base font-medium font-['Pretendard'] leading-7">
+                        건국대학교 메일 계정으로 인증해주세요
+                    </div>
+                </nav>
+            </div>
+        </div>
     )
 }
 
